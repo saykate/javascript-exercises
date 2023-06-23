1349f1a4b0fc743d4347de243a2ed38594b522b6
Katie Sayers <kate@student.dev-island.org>
2023-06-22 19:28:38 -1000

    Initial commit of first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let newStr = "";
+    for (let i = 0; i < num; i++) {
+        newStr += string;
+    }
+     return newStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, check) {
+   const newArr = arr.map(check) {
+    for (let i = 0; i < arr.length; i++);
+    if (check !== arr[i]) {
+        return arr[i]
+    };
+    return newArr
+}
 };
 
 // Do not edit below this line
