b73302949242bd357e0fe4894b0cfbb930da4bff
Katie Sayers <kate@student.dev-island.org>
2023-06-23 09:50:04 -1000

    Got exercise 7 to pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+     let tempF = (temp - 32) * (5 / 9);
+     return Math.round(tempF * 10) / 10 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+      let tempC = temp * (9 / 5) + 32;
+      return Math.round(tempC * 10) / 10  
 };
 
 // Do not edit below this line
