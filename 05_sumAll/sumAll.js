56b5f7340247ae735c655cf5cf9b8d5847e885bc
Katie Sayers <kate@student.dev-island.org>
2023-06-22 20:58:51 -1000

    Got exercise 5 to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR"
+    }
+    let sum = 0;
+    if (x > y) {
+        [x, y] = [y, x]
+    }
+    for (let i = x; i <= y; i++) {
+        sum += i; 
+    }
+    return sum
 };
 
 // Do not edit below this line
