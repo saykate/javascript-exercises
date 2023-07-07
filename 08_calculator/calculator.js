853c8b2d5270695180746a6adbe9da1e585bd98a
Katie Sayers <kate@student.dev-island.org>
2023-07-07 10:34:05 -1000

    calculator exercise complete with test error

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0
+  } else {
+
+    let sumArr = arr.reduce((total, current) => { 
+      return total + current}, 0)
+
+      return sumArr
+  }
+  
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  console.log('NUMS', nums)
+  let product = nums.reduce((total, current) => {
+    return total * current
+  }, 1); console.log(typeof product)
+};
+//something is wonky...
 
+const power = function(x, y) {
+  let num = 1;
+	for (let times = 0; times < y; times++) {
+      num *= x
+  }; return num
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(x) {
+  if (x === 0) {
+    return 1
+  } else {
+    let num = 1
+    for (let i = x; i > 0; i--) {
+      num *= i;
+    } return num
+  }
 	
 };
 
