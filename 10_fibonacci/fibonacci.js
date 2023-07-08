e332136752630c24514c88c215e985469a5d0283
Katie Sayers <kate@student.dev-island.org>
2023-07-07 15:54:41 -1000

    completed the 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0) {
+        return 'OOPS'
+    } else {
+        let previous = 0
+        let current = 1 
+        for (let i = 2; i <= input; i++) {
+            let temp = current; 
+            current += previous;
+            previous = temp;
+} return current
+};
 };
 
 // Do not edit below this line
