42bbcbfe692498743d35f5d34dc2f28939ff7926
Katie Sayers <kate@student.dev-island.org>
2023-07-11 12:08:45 -1000

    completed exercise 12 and passed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+    people.forEach(person => {
+    if (person.hasOwnProperty('yearOfDeath') === false) {
+        return person.age = ((new Date()).getFullYear()) - person.yearOfBirth
+    }  else {
+        return person.age = person.yearOfDeath - person.yearOfBirth
+    }
+});  
+console.log(people)
+    let oldest = people[0]
+    people.filter((person) => {
+        if (person.age > oldest.age) {
+            oldest = person
+        }
+    }); return oldest
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
